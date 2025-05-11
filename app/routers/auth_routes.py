2b9ba3dac2e3072967b2eb1ff62e8ca67966ca21
kulasekhar1816 <107290184+kulasekhar1816@users.noreply.github.com>
2025-05-11 20:21:51 +0530

    Update auth_routes.py
    Update auth_routes.py

--- app/routers/auth_routes.py
+++ app/routers/auth_routes.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 from sqlalchemy import select
 from .. import models, schemas, database, auth
-from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     prefix="/auth",
@@ -14,14 +13,14 @@ get_db = database.get_db
 
 # --- Register --- #
 @router.post("/register", response_model=schemas.UserBase)
-async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # Check if user already exists
-    result = await db.execute(select(models.User).where(models.User.username == user.username))
+    result = db.execute(select(models.User).where(models.User.username == user.username))
     existing_user = result.scalars().first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already registered")
     
-    result = await db.execute(select(models.User).where(models.User.email == user.email))
+    result = db.execute(select(models.User).where(models.User.email == user.email))
     existing_email = result.scalars().first()
     if existing_email:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -34,20 +33,19 @@ async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db))
     )
 
     db.add(db_user)
-    await db.commit()
-    await db.refresh(db_user)
+    db.commit()
+    db.refresh(db_user)
     return db_user
 
 # --- Login --- #
 @router.post("/login", response_model=schemas.Token)
-def login(form_data: schemas.UserLogin, db: AsyncSession = Depends(get_db)):
+def login(form_data: schemas.UserLogin, db: Session = Depends(get_db)):
     result = db.execute(select(models.User).where(models.User.username == form_data.username))
     user = result.scalars().first()
 
     if not user or not auth.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid username or password")
-    print("#################")
+
     user_id = user.id
-    print("user id: ", user_id)
     access_token = auth.create_access_token(data={"sub": str(user_id)})
     return {"access_token": access_token, "token_type": "bearer", "user_id": user_id}
